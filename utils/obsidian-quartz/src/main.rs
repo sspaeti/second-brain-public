73e701a78c2840866afd323363b5ed8c591e1aae
sspaeti <simon@ssp.sh>
2026-08-25 23:30:10 +0200

    add video support

--- utils/obsidian-quartz/src/main.rs
+++ utils/obsidian-quartz/src/main.rs
@@ -122,7 +122,7 @@ fn build_images_map(dir: &Path, images_map: &mut HashMap<String, PathBuf>) -> st
                 build_images_map(&path, images_map)?;
             } else {
                 if let Some(extension) = path.extension() {
-                    if extension == "png" || extension == "jpg" || extension == "gif" || extension == "webp" {
+                    if extension == "png" || extension == "jpg" || extension == "jpeg" || extension == "gif" || extension == "webp" || extension == "svg" || extension == "mp4" {
                         if let Some(file_name) = path.file_name() {
                             images_map.insert(file_name.to_str().unwrap().to_string(), path.clone());
                         }
